cb81d50034607ccf5e6b4ed132877e642f627818
Priscila Mordente <priscilamordente@gmail.com>
2022-07-19 11:08:31 -0300

    feito exercicios aula 7

---
+++ exercicio1/script.js
@@ -0,0 +1,35 @@
+/*
+
+# Exercícios A,B e C: */
+
+
+/* const nomeCompleto;
+let idade;
+console.log (typeof nomeCompleto)
+console.log (typeof idade) */
+
+
+/* 
+
+# Exercício D
+O código retornou com erro pois não houve atribuição de valores para as váriaveis 
+
+# Exercício E
+
+*/
+
+const nome = prompt ("Qual é o seu nome?")
+let idade = prompt ("Qual é a sua idade?")
+console.log (typeof nome)
+console.log (typeof idade)
+
+/* 
+
+# Exercício F:
+Não retornou erro. Observado que as duas variaveis retornaram ambas com string pois 
+foi utilizado a sintaxe do prompt que retorna strings 
+
+# Exercício G:
+*/
+
+console.log ("Olá" ,nome, "você tem" ,idade, "anos")
---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+let A = 25
+let B = 30
+let C;
+
+C=A
+A=B
+B=C
+
+console.log (A)
+console.log (B)
